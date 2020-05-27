5d79cdc8ab1ce7362e44149d8e3aa4b8b192b801
Sanjog chaulagain <SC11@SC11-2.local>
2020-05-28 00:20:36 +0545

    API call

--- forecast.js
+++ forecast.js
@@ -1,1 +1,35 @@
 const key = 'S2GEGDfzlx0mIREMDvgqG20L65JXL1j4';
+
+//Code to get city API call
+const getCity = async (city) => {
+
+    const citySearch = 'http://dataservice.accuweather.com/locations/v1/cities/search';
+    const query = `?apikey=${key}&q=${city}`;
+
+    const response = await fetch(citySearch + query);
+    const data = await response.json();
+
+    return data[0];
+};
+
+//Code to get current conditions API call
+const getCondition = async (locationcode) => {
+    const getCondition = 'http://dataservice.accuweather.com/currentconditions/v1/';
+    const query = `${locationcode}?apikey=S2GEGDfzlx0mIREMDvgqG20L65JXL1j4&`;
+
+    const response = await fetch(getCondition + query);
+    const data = await response.json();
+
+    return data[0];
+
+}
+
+getCity('istanbul')
+    .then(data => {
+        return getCondition(data.Key);
+    })
+    .then(data => console.log(data))
+    .catch(error => console.log(error));
+
+
+
