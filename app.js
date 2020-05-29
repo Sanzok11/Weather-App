972092b472caa89fdbfd2a9732e526bcfaa8d9da
Sanjog chaulagain <SC11@SC11-2.local>
2020-05-29 15:26:41 +0545

    Complete.

--- app.js
+++ app.js
@@ -1,10 +1,13 @@
 const form = document.querySelector('form');
 const card = document.querySelector('.card');
 const details = document.querySelector('.details');
+const daynight = document.querySelector('img.time');
+const logo = document.querySelector('.logo img')
 
 const updateUI = (data) => {
     const cD = data.cityDetails;
     const cC = data.cityCondition;
+    //console.log(cD, cC);
 
     //Changing Details
     details.innerHTML = ` <h5 class="my-3">${cD.EnglishName}</h5>
@@ -17,7 +20,20 @@ const updateUI = (data) => {
     if (card.classList.contains('d-none')) {
         card.classList.remove('d-none');
     }
+
+    //Adding daylight pic
+    if (cC.IsDayTime) {
+        daynight.setAttribute('src', 'img/day.svg');
+    }
+    else {
+        daynight.setAttribute('src', 'img/night.svg');
+    }
+
+    //Adding Weather-Icon
+    const logoimg = `img/icons/${cC.WeatherIcon}.svg`;
+    logo.setAttribute('src', logoimg);
 }
+
 
 const findCity = async (city) => {
     const cityDetails = await getCity(city);
